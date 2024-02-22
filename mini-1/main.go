b60a61de69f48cc6b4dd5aa12bf0a7144c9895f8
Komang Mardika <komang.mardika@gmail.com>
2024-02-22 21:38:08 +0800

    change to struct

--- mini-1/main.go
+++ mini-1/main.go
@@ -1,9 +1,3 @@
-/*
- * author: I Komang Mardika
- * email: komang.mardika@hotmail.com
- * app desc: mini project 1 for Markas Bali ft Kominfo Kelas Intensif 2024
- * first created on Monday, 19 feb 2024
- */
 package main
 
 import (
@@ -14,38 +8,113 @@ import (
 	"strings"
 )
 
-/* define default books */
-var books = []map[string]string{
-	{
-		"id":     "B-0001",
-		"title":  "Harry Potter And Sorcerer's Stone",
-		"status": "A",
-	},
-	{
-		"id":     "B-0002",
-		"title":  "Harry Potter And Chamber of Secret",
-		"status": "B",
-	},
-	{
-		"id":     "B-0003",
-		"title":  "Harry Potter And Prisoner of Azkaban",
-		"status": "A",
-	},
-	{
-		"id":     "B-0004",
-		"title":  "Harry Potter And Goblet of Fire",
-		"status": "A",
-	},
+type Book struct {
+	Id     string
+	Title  string
+	Status string
 }
 
-func main() {
-	/* call showMenu function for first time */
-	showMenu()
+func transactionBook(action string) {
+	var bookId string
+	fmt.Print("Masukkan ID buku yang ingin ")
+
+	if action == "B" {
+		fmt.Print("dipinjam : ")
+	} else {
+		fmt.Print("dikembalikan : ")
+	}
+
+	_, err := fmt.Scanln(&bookId)
+
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	} else {
+		bookId = strings.TrimSpace(bookId)
+
+		flag := false
+		for _, book := range books {
+			if book.Id == bookId {
+				fmt.Println("buku ditemukan berikut data nya")
+				fmt.Println("Book ID: ", book.Id)
+				fmt.Println("Book Title: ", book.Title)
+				fmt.Println("status buku sudah diubah")
+				if action == "B" {
+					book.Status = "B"
+				} else {
+					book.Status = "A"
+				}
+				flag = true
+				break
+			}
+		}
+
+		if !flag {
+			confirmation()
+		} else {
+			println("buku tidak ditemukan")
+			confirmation()
+		}
+	}
 }
-func showMenu() {
+func showAll(status string) {
+	var s string
+	for i := 0; i < len(books); i++ {
+		if status == books[i].Status {
+			fmt.Printf(`
+	id: %s
+	title: %s
+`, books[i].Id, books[i].Title)
+		} else {
+			if status == "all" {
+				if books[i].Status == "A" {
+					s = "Tersedia"
+				} else {
+					s = "Dipinjam"
+				}
+				fmt.Printf(`
+	id: %s
+	title: %s
+	status: %s
+`, books[i].Id, books[i].Title, s)
+			}
+		}
+
+	}
+}
+func addNewBook() {
+	var bookTitle string
+	var bookId string
+
+	fmt.Print("Masukkan Title buku baru : ")
+	_, err := fmt.Scanln(&bookTitle)
+	if err != nil {
+		fmt.Print("err")
+	}
+
+	index := len(books) - 1
+	parts := strings.Split(books[index].Id, "-")
+	number, _ := strconv.Atoi(parts[1])
+	number = number + 1
+
+	if number < 10 {
+		bookId = "B-000" + strconv.Itoa(number)
+	} else if number > 9 && number <= 99 {
+		bookId = "B-00" + strconv.Itoa(number)
+	} else if number > 99 && number <= 999 {
+		bookId = "B-0" + strconv.Itoa(number)
+	} else {
+		bookId = "B-" + strconv.Itoa(number)
+	}
+
+	books = append(books, Book{Id: bookId, Title: bookTitle, Status: "A"})
+	fmt.Println("Buku berhasil ditambahkan")
+	confirmation()
+}
+func menu() {
 	var menuChosen string
 
-	clearConsole()
+	clearScreen()
 
 	fmt.Printf(`
 Simple Perpustakaan App 
@@ -58,8 +127,7 @@ Simple Perpustakaan App
 	7. Keluar dari aplikasi
 `)
 	fmt.Print("Pilihan anda: ")
-	reader := bufio.NewReader(os.Stdin)
-	menuChosen, err := reader.ReadString('\n')
+	_, err := fmt.Scanln(&menuChosen)
 	if err != nil {
 		fmt.Println("Error reading input:", err)
 		return
@@ -69,11 +137,11 @@ Simple Perpustakaan App
 
 		switch menuChosen {
 		case "1":
-			showAllBooks("A")
+			showAll("A")
 		case "2":
-			showAllBooks("B")
+			showAll("B")
 		case "3":
-			showAllBooks("all")
+			showAll("all")
 		case "4":
 			addNewBook()
 		case "5":
@@ -87,16 +155,18 @@ Simple Perpustakaan App
 			fmt.Print("Anda tidak memilih menu yang tepat, ")
 		}
 
-		confirm()
+		confirmation()
 	}
-
 }
-
-func clearConsole() {
-	fmt.Print("\033[H\033[2J")
+func confirmation() {
+	if keyPress() {
+		menu()
+	} else {
+		fmt.Println("Terima kasih sudah menggunakan aplikasi ini")
+		return
+	}
 }
-
-func holdKey() bool {
+func keyPress() bool {
 	fmt.Println("tekan y untuk mengulang")
 	reader := bufio.NewReader(os.Stdin)
 	for {
@@ -109,104 +179,29 @@ func holdKey() bool {
 	}
 }
 
-func showAllBooks(param string) {
-	var status string
-	for i := 0; i < len(books); i++ {
-		if param == books[i]["status"] {
-			fmt.Printf(`
-	id: %s
-	title: %s
-`, books[i]["id"], books[i]["title"])
-		} else {
-			if param == "all" {
-				if books[i]["status"] == "A" {
-					status = "Tersedia"
-				} else {
-					status = "Dipinjam"
-				}
-				fmt.Printf(`
-	id: %s
-	title: %s
-	status: %s
-`, books[i]["id"], books[i]["title"], status)
-			}
-		}
-
-	}
+var books = []Book{
+	{
+		Id:     "B-00001",
+		Title:  "Harry Potter and Sorcerer's Stone",
+		Status: "A",
+	},
+	{
+		Id:     "B-00002",
+		Title:  "Harry Potter and Chamber of Secret",
+		Status: "A",
+	},
+	{
+		Id:     "B-00003",
+		Title:  "Harry Potter and Prisoner of Azkaban",
+		Status: "A",
+	},
 }
 
-func addNewBook() {
-	var bookId string
-	fmt.Print("Masukkan Title buku baru : ")
-	readerTitle := bufio.NewReader(os.Stdin)
-	bookTitle, _ := readerTitle.ReadString('\n')
-
-	index := len(books) - 1
-	parts := strings.Split(books[index]["id"], "-")
-	number, _ := strconv.Atoi(parts[1])
-	number = number + 1
-	if number < 10 {
-		bookId = "B-000" + strconv.Itoa(number)
-	} else if number > 9 && number <= 99 {
-		bookId = "B-00" + strconv.Itoa(number)
-	} else if number > 99 && number <= 999 {
-		bookId = "B-0" + strconv.Itoa(number)
-	} else {
-		bookId = "B-" + strconv.Itoa(number)
-	}
-
-	books = append(books, map[string]string{"id": bookId, "title": bookTitle, "status": "A"})
-	fmt.Println("Buku berhasil ditambahkan")
-	confirm()
+func main() {
+	// seed books
+	menu()
 }
 
-func transactionBook(param string) {
-	var bookId string
-	fmt.Print("Masukkan ID buku yang ingin ")
-	if param == "B" {
-		fmt.Print("dipinjam : ")
-	} else {
-		fmt.Print("dikembalikan : ")
-	}
-	reader := bufio.NewReader(os.Stdin)
-	bookId, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		return
-	} else {
-		bookId = strings.TrimSpace(bookId)
-
-		flag := false
-		for _, book := range books {
-			if book["id"] == bookId {
-				fmt.Println("buku ditemukan berikut data nya")
-				fmt.Println("Book ID: ", book["id"])
-				fmt.Println("Book Title: ", book["title"])
-				fmt.Println("status buku sudah diubah")
-				if param == "B" {
-					book["status"] = "B"
-				} else {
-					book["status"] = "A"
-				}
-				flag = true
-				break
-			}
-		}
-
-		if !flag {
-			confirm()
-		} else {
-			println("buku tidak ditemukan")
-			confirm()
-		}
-	}
-}
-
-func confirm() {
-	if holdKey() {
-		showMenu()
-	} else {
-		fmt.Println("Terima kasih sudah menggunakan aplikasi ini")
-		return
-	}
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
 }
